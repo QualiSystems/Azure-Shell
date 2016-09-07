73a139e14dd07a3b9f982ccc835126b33ae52c16
GilGald <gil.2006@gmail.com>
2016-09-07 17:37:37 +0300

    Created azure driver

---
+++ drivers/azure_shell/driver.py
@@ -0,0 +1,48 @@
+from cloudshell.shell.core.resource_driver_interface import ResourceDriverInterface
+
+
+class AzureShellDriver(ResourceDriverInterface):
+    def cleanup(self):
+        pass
+
+    def __init__(self):
+        """
+        ctor must be without arguments, it is created with reflection at run time
+        """
+        pass
+
+    def initialize(self, context):
+        pass
+
+    def deploy_resource(self, context, request):
+        pass
+
+    def PowerOn(self, context, ports):
+        pass
+
+    def PowerOff(self, context, ports):
+        pass
+
+    def PowerCycle(self, context, ports, delay):
+        pass
+
+    def remote_refresh_ip(self, context, ports, cancellation_context):
+        pass
+
+    def destroy_vm_only(self, context, ports):
+        pass
+
+    def PrepareConnectivity(self, context, request):
+        pass
+
+    def CleanupConnectivity(self, context, request):
+        pass
+
+    def GetApplicationPorts(self, context, ports):
+        pass
+
+    def get_inventory(self, context):
+        pass
+
+
+
