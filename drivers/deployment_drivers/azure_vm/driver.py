f7e36a35b15d955af04459e3c2a288fb4883d78c
GilGald <gil.2006@gmail.com>
2016-09-12 13:54:29 +0300

    Created driver side deployment option.

---
+++ drivers/deployment_drivers/azure_vm/driver.py
@@ -0,0 +1,15 @@
+from cloudshell.shell.core.resource_driver_interface import ResourceDriverInterface
+
+
+class DeployAzureVM(ResourceDriverInterface):
+    def __init__(self):
+        pass
+
+    def cleanup(self):
+        pass
+
+    def initialize(self, context):
+        pass
+
+    def Deploy(self,context,Name=None):
+        pass
