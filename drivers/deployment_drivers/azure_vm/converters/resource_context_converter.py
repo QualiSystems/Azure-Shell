cfb59ec5153e56e3768378b883c57de4ef69d919
GilGald <gil.2006@gmail.com>
2016-09-13 14:51:25 +0300

    added convert

--- drivers/deployment_drivers/azure_vm/converters/resource_context_converter.py
+++ drivers/deployment_drivers/azure_vm/converters/resource_context_converter.py
@@ -1,3 +1,6 @@
+from cloudshell.cp.azure.models.deploy_azure_vm_resource_model import DeployAzureVMResourceModel
+
+
 class ResourceContextConverter(object):
     def __init__(self):
         pass
@@ -10,23 +13,18 @@ class ResourceContextConverter(object):
         :param deployment_credentials:
         :return:
         """
-        raise Exception("this is an exception from  resource_context_to_deployment_resource_model")
-
-
-        # deployedResource = DeployAWSEc2AMIInstanceResourceModel()
-        # deployedResource.aws_ami_id = resource.attributes['AWS AMI Id']
-        # deployedResource.cloud_provider = resource.attributes['Cloud Provider']
-        # deployedResource.storage_iops = resource.attributes['Storage IOPS']
-        # deployedResource.storage_size = resource.attributes['Storage Size']
-        # deployedResource.storage_type = resource.attributes['Storage Type']
-        # deployedResource.instance_type = resource.attributes['Instance Type']
-        # deployedResource.wait_for_ip = resource.attributes['Wait for IP']
-        # deployedResource.autoload = resource.attributes['Autoload']
-        # deployedResource.inbound_ports = resource.attributes['Inbound Ports']
-        # deployedResource.outbound_ports = resource.attributes['Outbound Ports']
-        # deployedResource.wait_for_credentials = self._convert_to_bool(resource.attributes['Wait for Credentials'])
-        # deployedResource.add_public_ip = self._convert_to_bool(resource.attributes['Add Public IP'])
-        # deployedResource.add_elastic_ip = resource.attributes['Add Elastic IP']
-        # deployedResource.root_volume_name = resource.attributes['Root Volume Name']
-        # deployedResource.user = deployment_credentiales['user']
-        # deployedResource.wait_for_status_check = resource.attributes['Wait for Status Check']
+        deployed_resource = DeployAzureVMResourceModel()
+        deployed_resource.group_name = resource.attributes['AWS AMI Id']
+        deployed_resource.vm_name = resource.attributes['AWS AMI Id']
+        deployed_resource.cloud_provider = resource.attributes['AWS AMI Id']
+        deployed_resource.instance_type = resource.attributes['AWS AMI Id']
+        deployed_resource.wait_for_ip = resource.attributes['AWS AMI Id']
+        deployed_resource.autoload = resource.attributes['AWS AMI Id']
+        deployed_resource.add_public_ip = resource.attributes['AWS AMI Id']
+        deployed_resource.inbound_ports = resource.attributes['AWS AMI Id']
+        deployed_resource.outbound_ports = resource.attributes['AWS AMI Id']
+        deployed_resource.public_ip_type = resource.attributes['AWS AMI Id']
+        deployed_resource.image_publisher = resource.attributes['AWS AMI Id']
+        deployed_resource.image_offer = resource.attributes['AWS AMI Id']
+        deployed_resource.image_sku = resource.attributes['AWS AMI Id']
+        deployed_resource.disk_type = resource.attributes['AWS AMI Id']
