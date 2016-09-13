53664df58889bdfe052e13a30f8ec24e778b7cb8
GilGald <gil.2006@gmail.com>
2016-09-13 15:26:27 +0300

    Added data to the converter

--- drivers/deployment_drivers/azure_vm/converters/resource_context_converter.py
+++ drivers/deployment_drivers/azure_vm/converters/resource_context_converter.py
@@ -14,17 +14,17 @@ class ResourceContextConverter(object):
         :return:
         """
         deployed_resource = DeployAzureVMResourceModel()
-        deployed_resource.group_name = resource.attributes['AWS AMI Id']
-        deployed_resource.vm_name = resource.attributes['AWS AMI Id']
-        deployed_resource.cloud_provider = resource.attributes['AWS AMI Id']
-        deployed_resource.instance_type = resource.attributes['AWS AMI Id']
-        deployed_resource.wait_for_ip = resource.attributes['AWS AMI Id']
-        deployed_resource.autoload = resource.attributes['AWS AMI Id']
-        deployed_resource.add_public_ip = resource.attributes['AWS AMI Id']
-        deployed_resource.inbound_ports = resource.attributes['AWS AMI Id']
-        deployed_resource.outbound_ports = resource.attributes['AWS AMI Id']
-        deployed_resource.public_ip_type = resource.attributes['AWS AMI Id']
-        deployed_resource.image_publisher = resource.attributes['AWS AMI Id']
-        deployed_resource.image_offer = resource.attributes['AWS AMI Id']
-        deployed_resource.image_sku = resource.attributes['AWS AMI Id']
-        deployed_resource.disk_type = resource.attributes['AWS AMI Id']
+        deployed_resource.group_name = "" #needs to be auto generated
+        deployed_resource.vm_name = ""#needs to be auto generated
+        deployed_resource.cloud_provider = resource.attributes['Cloud Provider']
+        deployed_resource.instance_type = resource.attributes['Instance Type']
+        deployed_resource.wait_for_ip = resource.attributes['awsAmiId']
+        deployed_resource.autoload = resource.attributes['Wait For Ip']
+        deployed_resource.add_public_ip = resource.attributes['Add Public Ip']
+        deployed_resource.inbound_ports = resource.attributes['Inbound Ports']
+        deployed_resource.outbound_ports = resource.attributes['Outbound Ports']
+        deployed_resource.public_ip_type = resource.attributes['Public Ip Type']
+        deployed_resource.image_publisher = resource.attributes['Image Publisher']
+        deployed_resource.image_offer = resource.attributes['Image Offer']
+        deployed_resource.image_sku = resource.attributes['Image SKU']
+        deployed_resource.disk_type = resource.attributes['Disk Type']
