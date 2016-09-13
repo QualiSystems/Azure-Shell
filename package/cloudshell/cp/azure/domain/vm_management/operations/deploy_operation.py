706a4bb9d9b1deff1f17495b26270defdbee54d6
igoro1975 <igoro1975@gmail.com>
2016-09-13 16:18:38 +0300

    Push for Gil

--- package/cloudshell/cp/azure/domain/vm_management/operations/deploy_operation.py
+++ package/cloudshell/cp/azure/domain/vm_management/operations/deploy_operation.py
@@ -1,3 +1,7 @@
+from azure.mgmt.compute import VirtualMachine, OSProfile, HardwareProfile, NetworkProfile, StorageProfile, OSDisk, \
+    VirtualHardDisk, ImageReference, VirtualMachineSizeTypes, NetworkInterfaceReference, CachingTypes, \
+    DiskCreateOptionTypes
+
 from cloudshell.cp.azure.models.deploy_result_model import DeployResult
 
 
@@ -22,7 +26,42 @@ class DeployAzureVMOperation(object):
 
         deploy_data = compute_client.virtual_machines.create_or_update(
             azure_vm_deployment_model.group_name,
-            azure_vm_deployment_model.vm_name
+            azure_vm_deployment_model.vm_name,
+            VirtualMachine(
+                location='TBD',
+                os_profile=OSProfile(
+                    computer_name='TBD',
+                    admin_username='TBD',
+                    admin_password='TBD'
+                ),
+                hardware_profile=HardwareProfile(
+                    vm_size=VirtualMachineSizeTypes.basic_a0
+                ),
+                network_profile=NetworkProfile(
+                    network_interfaces=[
+                        NetworkInterfaceReference(
+                            id='TBD'
+                        ),
+                    ]
+                ),
+                storage_profile=StorageProfile(
+                    os_disk=OSDisk(
+                        caching=CachingTypes.none,
+                        create_option=DiskCreateOptionTypes.from_image,
+                        name='TBD',
+                        vhd=VirtualHardDisk(
+                            uri='TBD',  # the VM name
+                            )
+                        )
+                    ),
+                    image_reference=ImageReference(
+                        publisher=azure_vm_deployment_model.image_publisher,
+                        offer=azure_vm_deployment_model.image_offer,
+                        sku=azure_vm_deployment_model.image_sku,
+                        version='TBD'
+                    )
+                )
+            )
         )
 
         return DeployResult()
