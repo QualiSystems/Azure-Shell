06acd9c698cc0e78b135ae0bc84ccb221d6556d8
GilGald <gil.2006@gmail.com>
2016-09-21 14:31:37 +0300

    fixing tests

--- package/tests/test_deploy_operation.py
+++ package/tests/test_deploy_operation.py
@@ -3,6 +3,7 @@ import uuid
 from mock import Mock
 from cloudshell.cp.azure.domain.services.network_service import NetworkService
 from cloudshell.cp.azure.domain.services.storage_service import StorageService
+from cloudshell.cp.azure.domain.services.tags import TagService
 from cloudshell.cp.azure.domain.services.virtual_machine_service import VirtualMachineService
 from cloudshell.cp.azure.domain.vm_management.operations.deploy_operation import DeployAzureVMOperation
 from cloudshell.cp.azure.models.azure_cloud_provider_resource_model import AzureCloudProviderResourceModel
@@ -16,10 +17,12 @@ class TestAzureShell(TestCase):
         self.storage_service = StorageService(Mock())
         self.vm_service = VirtualMachineService(Mock(), Mock())
         self.network_service = NetworkService(Mock())
+        self.tag_service = TagService()
         self.deploy_operation = DeployAzureVMOperation(logger=self.logger,
                                                        vm_service=self.vm_service,
                                                        network_service=self.network_service,
-                                                       storage_service=self.storage_service)
+                                                       storage_service=self.storage_service,
+                                                       tags_service=self.tag_service)
 
     def test_deploy_operation_deploy_result(self):
         """
@@ -32,7 +35,6 @@ class TestAzureShell(TestCase):
         self.storage_service.create_storage_account = Mock(return_value=True)
         self.network_service.create_network = Mock(return_value="The Created NIC")
         self.vm_service.create_vm = Mock(return_value=Mock())
-        self.vm_service.get_vm = Mock(return_value=Mock())
 
         # Act
         self.deploy_operation.deploy(DeployAzureVMResourceModel(), AzureCloudProviderResourceModel(), uuid.uuid4())
@@ -42,4 +44,3 @@ class TestAzureShell(TestCase):
         self.assertTrue(TestHelper.CheckMethodCalledXTimes(self.storage_service.create_storage_account))
         self.assertTrue(TestHelper.CheckMethodCalledXTimes(self.network_service.create_network))
         self.assertTrue(TestHelper.CheckMethodCalledXTimes(self.vm_service.create_vm))
-        self.assertTrue(TestHelper.CheckMethodCalledXTimes(self.vm_service.get_vm))
