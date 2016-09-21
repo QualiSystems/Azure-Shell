62a38d7af19b094bd17ce4977611f88326b691c1
GilGald <gil.2006@gmail.com>
2016-09-21 11:59:35 +0300

    added tests

---
+++ package/tests/test_deploy_operation.py
@@ -0,0 +1,34 @@
+from unittest import TestCase
+
+from mock import Mock
+
+from cloudshell.cp.azure.domain.services.network_service import NetworkService
+from cloudshell.cp.azure.domain.services.storage_service import StorageService
+from cloudshell.cp.azure.domain.services.virtual_machine_service import VirtualMachineService
+from cloudshell.cp.azure.domain.vm_management.operations.deploy_operation import DeployAzureVMOperation
+from cloudshell.cp.azure.models.azure_cloud_provider_resource_model import AzureCloudProviderResourceModel
+from cloudshell.cp.azure.models.deploy_azure_vm_resource_model import DeployAzureVMResourceModel
+
+
+class TestAzureShell(TestCase):
+    def setUp(self):
+        self.logger = Mock()
+        self.storage_service = StorageService(Mock())
+        self.vm_service = VirtualMachineService(Mock(), Mock())
+        self.network_service = NetworkService(Mock())
+        self.deploy_operation = DeployAzureVMOperation(logger=self.logger,
+                                                       vm_service=self.vm_service,
+                                                       network_service=self.network_service,
+                                                       storage_service=self.storage_service)
+
+    def test_deploy_operation_deploy_result(self):
+        self.vm_service.create_resource_group = Mock(return_value=True)
+        self.storage_service.create_storage_account = Mock(return_value=True)
+        self.network_service.create_network = Mock(return_value="The Created NIC")
+        self.vm_service.create_vm = Mock(return_value="The Created Resource")
+        self.vm_service.get_vm = Mock(return_value=Mock())
+        self.deploy_operation.deploy(DeployAzureVMResourceModel(),AzureCloudProviderResourceModel())
+
+
+
+
