fccf40d3af705bea2796db86df78cbb9fbdbe051
Anthony Piddubny <anton.p@qualisystems.com>
2016-09-27 19:04:49 +0300

    Task 5 Power On/Off commands on Azure VM

---
+++ package/cloudshell/cp/azure/domain/vm_management/operations/power_operation.py
@@ -0,0 +1,29 @@
+class PowerAzureVMOperation(object):
+    def __init__(self, logger, vm_service):
+        """
+        :param logger:
+        :param cloudshell.cp.azure.domain.services.virtual_machine_service.VirtualMachineService vm_service:
+        :return:
+        """
+        self.logger = logger
+        self.vm_service = vm_service
+
+    def power_on(self, compute_client, resource_group_name, vm_name):
+        """Power on Azure VM instance
+
+        :param azure.mgmt.compute.compute_management_client.ComputeManagementClient compute_client:
+        :param resource_group_name: The name of the resource group.
+        :param vm_name: The name of the virtual machine.
+        :return
+        """
+        return self.vm_service.start_vm(compute_client, resource_group_name, vm_name)
+
+    def power_off(self, compute_client, resource_group_name, vm_name):
+        """Power off Azure VM instance
+
+        :param azure.mgmt.compute.compute_management_client.ComputeManagementClient compute_client:
+        :param resource_group_name: The name of the resource group.
+        :param vm_name: The name of the virtual machine.
+        :return
+        """
+        return self.vm_service.stop_vm(compute_client, resource_group_name, vm_name)
