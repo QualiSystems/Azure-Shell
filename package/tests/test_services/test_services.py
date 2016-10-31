f50bd2f47d22f96ba65802c8a221c7aa1095a228
Anthony Piddubny <anton.p@qualisystems.com>
2016-10-31 16:35:03 +0200

    add unit tests for the security group service

--- package/tests/test_services/test_services.py
+++ package/tests/test_services/test_services.py
@@ -9,6 +9,7 @@ from cloudshell.cp.azure.domain.services.network_service import NetworkService
 from cloudshell.cp.azure.domain.services.key_pair import KeyPairService
 from cloudshell.cp.azure.domain.services.storage_service import StorageService
 from cloudshell.cp.azure.domain.services.virtual_machine_service import VirtualMachineService
+from cloudshell.cp.azure.domain.services.security_group import SecurityGroupService
 
 
 class TestStorageService(TestCase):
@@ -283,3 +284,141 @@ class TestKeyPairService(TestCase):
             file_name=self.key_pair_service.SSH_PRIVATE_KEY_NAME)
 
         self.assertIs(key_pair, mocked_key_pair)
+
+
+class TestSecurityGroupService(TestCase):
+    def setUp(self):
+        self.security_group_service = SecurityGroupService()
+        self.group_name = "test_group_name"
+        self.security_group_name = "teststoragename"
+        self.network_client = mock.MagicMock()
+
+    def test_rule_priority_generator(self):
+        """Check that method creates generator started from the given value plus increase step"""
+        start_value = 100
+        expected_values = [start_value + self.security_group_service.RULE_PRIORITY_INCREASE_STEP,
+                           start_value + self.security_group_service.RULE_PRIORITY_INCREASE_STEP * 2,
+                           start_value + self.security_group_service.RULE_PRIORITY_INCREASE_STEP * 3,
+                           start_value + self.security_group_service.RULE_PRIORITY_INCREASE_STEP * 4]
+
+        # Act
+        generator = self.security_group_service._rule_priority_generator(start_value)
+
+        # Verify
+        generated_values = [next(generator) for _ in xrange(4)]
+        self.assertEqual(expected_values, generated_values)
+
+    def test_list_network_security_group(self):
+        """Check that method calls azure network client to get list of NSGs and converts them into list"""
+        # Act
+        security_groups = self.security_group_service.list_network_security_group(
+            network_client=self.network_client,
+            group_name=self.group_name)
+
+        # Verify
+        self.network_client.network_security_groups.list.assert_called_once_with(self.group_name)
+        self.assertIsInstance(security_groups, list)
+
+    @mock.patch("cloudshell.cp.azure.domain.services.security_group.NetworkSecurityGroup")
+    def test_create_network_security_group(self, nsg_class):
+        """Check that method calls azure network client to create NSG and returns it result"""
+        region = mock.MagicMock()
+        tags = mock.MagicMock()
+        nsg_class.return_value = nsg_model = mock.MagicMock()
+
+        # Act
+        nsg = self.security_group_service.create_network_security_group(
+            network_client=self.network_client,
+            group_name=self.group_name,
+            security_group_name=self.security_group_name,
+            region=region,
+            tags=tags)
+
+        # Verify
+        self.network_client.network_security_groups.create_or_update.assert_called_once_with(
+            resource_group_name=self.group_name,
+            network_security_group_name=self.security_group_name,
+            parameters=nsg_model)
+
+        self.assertEqual(nsg, self.network_client.network_security_groups.create_or_update().result())
+
+    @mock.patch("cloudshell.cp.azure.domain.services.security_group.SecurityRule")
+    def test_prepare_security_group_rule(self, security_rule_class):
+        """Check that method returns SecurityRule model"""
+        security_rule_class.return_value = security_rule = mock.MagicMock()
+        rule_data = mock.MagicMock()
+        private_vm_ip = mock.MagicMock()
+        priority = mock.MagicMock()
+
+        # Act
+        prepared_rule = self.security_group_service._prepare_security_group_rule(
+            rule_data=rule_data,
+            private_vm_ip=private_vm_ip,
+            priority=priority)
+
+        # Verify
+        self.assertEqual(prepared_rule, security_rule)
+
+    def test_create_network_security_group_rules_without_existing_rules(self):
+        """Check that method will call network_client for NSG rules creation starting from default priority"""
+        rule_data = mock.MagicMock()
+        inbound_rules = [rule_data]
+        private_vm_ip = mock.MagicMock()
+        rule_model = mock.MagicMock()
+        self.network_client.security_rules.list.return_value = []
+        self.security_group_service._prepare_security_group_rule = mock.MagicMock(return_value=rule_model)
+
+        # Act
+        self.security_group_service.create_network_security_group_rules(
+            network_client=self.network_client,
+            group_name=self.group_name,
+            security_group_name=self.security_group_name,
+            inbound_rules=inbound_rules,
+            private_vm_ip=private_vm_ip)
+
+        # Verify
+        self.security_group_service._prepare_security_group_rule.assert_called_once_with(
+            priority=self.security_group_service.RULE_DEFAULT_PRIORITY,
+            private_vm_ip=private_vm_ip,
+            rule_data=rule_data)
+
+        self.network_client.security_rules.create_or_update.assert_called_with(
+            network_security_group_name=self.security_group_name,
+            resource_group_name=self.group_name,
+            security_rule_name=rule_model.name,
+            security_rule_parameters=rule_model)
+
+    def test_create_network_security_group_rules_with_existing_rules(self):
+        """Check that method will call network_client for NSG rules creation starting from last rule priority"""
+        rule_data = mock.MagicMock()
+        inbound_rules = [rule_data]
+        private_vm_ip = mock.MagicMock()
+        rule_model = mock.MagicMock()
+        max_existing_priority = 100500
+
+        self.network_client.security_rules.list.return_value = [
+            mock.MagicMock(priority=5000),
+            mock.MagicMock(priority=max_existing_priority),
+            mock.MagicMock(priority=100000)]
+
+        self.security_group_service._prepare_security_group_rule = mock.MagicMock(return_value=rule_model)
+
+        # Act
+        self.security_group_service.create_network_security_group_rules(
+            network_client=self.network_client,
+            group_name=self.group_name,
+            security_group_name=self.security_group_name,
+            inbound_rules=inbound_rules,
+            private_vm_ip=private_vm_ip)
+
+        # Verify
+        self.security_group_service._prepare_security_group_rule.assert_called_once_with(
+            priority=max_existing_priority + self.security_group_service.RULE_PRIORITY_INCREASE_STEP,
+            private_vm_ip=private_vm_ip,
+            rule_data=rule_data)
+
+        self.network_client.security_rules.create_or_update.assert_called_with(
+            network_security_group_name=self.security_group_name,
+            resource_group_name=self.group_name,
+            security_rule_name=rule_model.name,
+            security_rule_parameters=rule_model)
