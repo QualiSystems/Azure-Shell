55bdbf09476e803a0571685da57043e4854b84f1
GilGald <gil.2006@gmail.com>
2016-11-02 12:15:15 +0200

    OperationsHelper

--- package/cloudshell/cp/azure/common/operations_helper.py
+++ package/cloudshell/cp/azure/common/operations_helper.py
@@ -3,9 +3,6 @@ import uuid
 
 class OperationsHelper:
 
-    def __init__(self):
-        pass
-
     @staticmethod
     def generate_name(name):
         return name.replace(" ", "") + ((str(uuid.uuid4())).replace("-", ""))[0:8]
