274fd15c2cc2d58ccc9379234eb92223997ffec0
GilGald <gil.2006@gmail.com>
2016-11-08 15:34:01 +0200

    updated tests

--- package/tests/test_operations/test_prepare_connectivity.py
+++ package/tests/test_operations/test_prepare_connectivity.py
@@ -4,6 +4,7 @@ import mock
 import jsonpickle
 
 from cloudshell.cp.azure.common.deploy_data_holder import DeployDataHolder
+from cloudshell.cp.azure.domain.services.security_group import SecurityGroupService
 
 from tests.helpers.test_helper import TestHelper
 
@@ -29,7 +30,7 @@ class TestPrepareConnectivity(TestCase):
         self.network_service = NetworkService()
         self.tag_service = TagService()
         self.key_pair_service = KeyPairService(storage_service=self.storage_service)
-        self.security_group_service = MagicMock()
+        self.security_group_service = SecurityGroupService()
         self.logger = MagicMock()
 
         self.prepare_connectivity_operation = PrepareConnectivityOperation(
@@ -49,7 +50,8 @@ class TestPrepareConnectivity(TestCase):
         self.network_service.get_virtual_network_by_tag = MagicMock()
         self.storage_service.create_storage_account = MagicMock()
         self.vm_service.create_resource_group = MagicMock()
-
+        self.security_group_service.create_network_security_group = Mock(return_value=Mock())
+        self.security_group_service.create_network_security_group_custom_rule = Mock()
 
         req = MagicMock()
         action = MagicMock()
@@ -61,7 +63,7 @@ class TestPrepareConnectivity(TestCase):
         prepare_connectivity_request = req
         # Act
         result = Mock()
-        result.wait=Mock()
+        result.wait = Mock()
 
         network_client = MagicMock()
         network_client.subnets.create_or_update = Mock(return_value=result)
@@ -86,8 +88,12 @@ class TestPrepareConnectivity(TestCase):
 
         # key pair created
         self.assertTrue(TestHelper.CheckMethodCalledXTimes(self.key_pair_service.save_key_pair))
-        self.assertTrue(TestHelper.CheckMethodCalledXTimes(network_client.virtual_networks.list ))
+        self.assertTrue(TestHelper.CheckMethodCalledXTimes(network_client.virtual_networks.list))
         self.assertTrue(TestHelper.CheckMethodCalledXTimes(network_client.subnets.create_or_update))
+
+        # check the creation of the NSG
+        self.assertTrue(TestHelper.CheckMethodCalledXTimes(self.security_group_service.create_network_security_group))
+        self.assertTrue(TestHelper.CheckMethodCalledXTimes(self.security_group_service.create_network_security_group_custom_rule,2))
 
     def test_extract_cidr_throws_error(self):
         action = MagicMock()
