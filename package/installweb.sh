46cd025a2b02abaef81a3bbb554bb2aa2b08283e
GilGald <gil.2006@gmail.com>
2017-02-14 12:04:29 +0200

    added script

---
+++ package/installweb.sh
@@ -0,0 +1,6 @@
+sudo yum -y install httpd
+sudo firewall-cmd --permanent --add-port=80/tcp
+sudo firewall-cmd --permanent --add-port=443/tcp
+sudo firewall-cmd --reload
+sudo systemctl start httpd
+sudo systemctl enable httpd
