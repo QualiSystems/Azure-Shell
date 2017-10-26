ed791e2de36f6e390d2559619640a4073cdaf10c
Alex Azarh <Alex.az@qualisystems.com>
2017-10-26 09:44:13 +0300

    updated to latest 8.2 cloudshell automation api

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -3,11 +3,11 @@ if [ "${TRAVIS_BRANCH}" = "master" ]
 then
     pip install "cloudshell-core>=2.2.0,<2.3.0"
     pip install "cloudshell-shell-core>=3.1.0,<3.2.0"
-    pip install "cloudshell-automation-api>=8.0.0.0,<8.1.0.0"
+    pip install "cloudshell-automation-api>=8.2.0.0,<8.3.0.0"
 else
     pip install "cloudshell-core>=2.2.0,<2.3.0" --extra-index-url https://testpypi.python.org/simple
     pip install "cloudshell-shell-core>=3.1.0,<3.2.0" --extra-index-url https://testpypi.python.org/simple
-    pip install "cloudshell-automation-api>=8.0.0.0,<8.1.0.0" --extra-index-url https://testpypi.python.org/simple
+    pip install "cloudshell-automation-api>=8.2.0.0,<8.3.0.0" --extra-index-url https://testpypi.python.org/simple
 fi
 
 pip install -r test_requirements.txt
