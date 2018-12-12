77a04afc751f9eb67f2883a885f2a7637238c151
AdamSharon <quali.adam.sharon@gmail.com>
2018-12-12 16:18:29 +0200

    first check in for the retry on retryable error problem

--- package/cloudshell/cp/azure/common/helpers/retrying_helpers.py
+++ package/cloudshell/cp/azure/common/helpers/retrying_helpers.py
@@ -1,7 +1,13 @@
 import traceback
 
 from msrest.exceptions import ClientRequestError
+from msrestazure.azure_exceptions import CloudError
 from requests.packages.urllib3.exceptions import ConnectionError
+
+
+retryable_error_string = "RetryableError"
+retryable_error_timeout = 180000
+retryable_error_max_attempts = 3
 
 
 def retry_if_connection_error(exception):
@@ -12,9 +18,15 @@ def retry_if_connection_error(exception):
         exception)
 
 
+def retry_if_retryable_error(exception):
+    """Return True if we should retry (in this case when it's an RetryableError), False otherwise
+    :param exception:
+    """
+    return isinstance(exception, CloudError) and (exception.error.__contains__(retryable_error_string) or
+                                                  exception.message.__contains__(retryable_error_string))
+
+
 def is_pool_closed_error(exception):
     execption_message_list = traceback.format_exception_only(type(exception), exception)
     execption_message = "".join(execption_message_list)
     return "pool is closed" in execption_message.lower()
-
-
