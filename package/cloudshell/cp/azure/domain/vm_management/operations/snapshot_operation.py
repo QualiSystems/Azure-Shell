e18b7b4cd24326472d791313093478a93aa81e2d
Nahum Timerman <ntimerman@gmail.com>
2019-02-19 11:52:25 +0200

    # Yet another commit: Committing Gil's disk_type and message output for save snapshot Every deployed app will now return output Gets disk type to determine HDD /SSD Output is according to format determined by Alex. Ready for use by blueprint command

--- package/cloudshell/cp/azure/domain/vm_management/operations/snapshot_operation.py
+++ package/cloudshell/cp/azure/domain/vm_management/operations/snapshot_operation.py
@@ -2,7 +2,14 @@ from azure.mgmt.compute.models import Snapshot, StorageAccountTypes, CreationDat
 
 
 class SnapshotOperation:
-    def __init__(self, vm_service):
+    def __init__(self, vm_service, task_service):
+        """
+
+        :param vm_service:
+        :param task_service: cloudshell.cp.azure.domain.services.task_waiter.TaskWaiterService
+        :return: azure.mgmt.compute.models.Snapshot
+        """
+        self.task_service = task_service
         self.vm_service = vm_service
 
     def save(self,
@@ -11,21 +18,32 @@ class SnapshotOperation:
              instance_name,
              destination_resource_group,
              source_resource_group,
-             snapshot_name_prefix):
+             snapshot_name_prefix,
+             cancellation_context,
+             logger,
+             disk_type='Standard_LRS'):
+        """
+        :return:  azure.mgmt.compute.models.Snapshot
+        """
+
         vm = self.vm_service.get_vm(azure_clients.compute_client, source_resource_group, instance_name)
         disk_name = vm.storage_profile.os_disk.name
 
         managed_disk = azure_clients.compute_client.disks.get(source_resource_group, disk_name)
 
-        async_snapshot_creation = azure_clients.compute_client.snapshots.create_or_update(
+        snapshot_poller = azure_clients.compute_client.snapshots.create_or_update(
             destination_resource_group,
             "{0}{1}".format(snapshot_name_prefix, instance_name),
             Snapshot(location=cloud_provider_model.region,
-                     account_type=StorageAccountTypes.standard_lrs,
+                     account_type=disk_type,
                      creation_data=CreationData(create_option=DiskCreateOption.copy,
                                                 source_uri=managed_disk.id))
         )
 
-        snapshot = async_snapshot_creation.result()
+        self.task_service.wait_for_task(operation_poller=snapshot_poller,
+                                        cancellation_context=cancellation_context,
+                                        logger=logger)
+
+        snapshot = snapshot_poller.result()
 
         return snapshot
