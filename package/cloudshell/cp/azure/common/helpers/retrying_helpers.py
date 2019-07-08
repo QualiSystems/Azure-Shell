7c37e9f388d2c8bfe8d2edcd6ff658faa267b40a
Nahum Timerman <ntimerman@gmail.com>
2019-07-08 16:52:20 +0300

    added retry mechanism to cover azure retryableerror

--- package/cloudshell/cp/azure/common/helpers/retrying_helpers.py
+++ package/cloudshell/cp/azure/common/helpers/retrying_helpers.py
@@ -1,7 +1,12 @@
 import traceback
 
 from msrest.exceptions import ClientRequestError
+from msrestazure.azure_exceptions import CloudError
 from requests.packages.urllib3.exceptions import ConnectionError
+
+retryable_error_string = "retryable"
+retryable_wait_time = 2000
+retryable_error_max_attempts = 20
 
 
 def retry_if_connection_error(exception):
@@ -17,4 +22,15 @@ def is_pool_closed_error(exception):
     execption_message = "".join(execption_message_list)
     return "pool is closed" in execption_message.lower()
 
+
+def retry_if_retryable_error(exception):
+    """Return True if we should retry (in this case when it's an RetryableError), False otherwise
+    :param exceptions.Exception exception:
+    """
+    return isinstance(exception, CloudError) and _is_retryable_error_message(exception)
+
+
+def _is_retryable_error_message(exception):
+    error_has_retryable_in_message = retryable_error_string.lower() in exception.message.lower()
+    return error_has_retryable_in_message
 
--- package/cloudshell/cp/azure/domain/networking_management/operations/add_route_operation.py
+++ package/cloudshell/cp/azure/domain/networking_management/operations/add_route_operation.py
@@ -1,4 +1,6 @@
-from azure.mgmt.network.models import RouteNextHopType
+from retrying import retry
+
+import cloudshell.cp.azure.common.helpers.retrying_helpers as retrying_helpers
 from cloudshell.cp.azure.models.azure_cloud_provider_resource_model import AzureCloudProviderResourceModel
 from cloudshell.cp.azure.models.deploy_azure_vm_resource_models import RouteTableRequestResourceModel
 
@@ -11,8 +13,15 @@ class AddRouteOperation(object):
         """
         self.network_service= network_service
 
-    def create_route_table(self, route_table_request, cloud_provider_model,
-                           network_client, sandbox_id, subnet_lcoker):
+    @retry(stop_max_attempt_number=retrying_helpers.retryable_error_max_attempts,
+           wait_fixed=retrying_helpers.retryable_wait_time,
+           retry_on_exception=retrying_helpers.retry_if_retryable_error)
+    def create_route_table(self,
+                           route_table_request,
+                           cloud_provider_model,
+                           network_client,
+                           sandbox_id,
+                           subnet_lcoker):
 
         """
         :param RouteTableRequestResourceModel route_table_request:
